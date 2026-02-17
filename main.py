fb113fd10f2e25816a03fc44a11a1f98857727a2
WiltonGx <gabrielwilton83@gmail.com>
2026-02-16 23:03:25 -0400

    teste com cli

--- main.py
+++ main.py
@@ -1,9 +1,24 @@
-def solicitar_cpf (): 
-    cpf = input("Digite o CPF: ")
-    return cpf
+import csv
 
-print("Iniciando Sistema...")
+nome_digitado = input("Digite o nome: ")
+cpf_digitado = input("Digite o CPF: ")
 
-resultado = solicitar_cpf()
+aluno={
+    "Nome completo":nome_digitado,
+    "CPF": cpf_digitado,
+    "CEP": "78110-000",
+    "Nascimento": "24/11/2005",
+    "Responsável": "Pedro Augusto Nela",
+    "Telefone": "(65) 99999-9999",
+    "Série": "1º Ano do Ensino médio"
+}
 
-print(resultado)
+colunas = ["Nome completo", "CPF", "CEP", "Nascimento", "Responsável", "Telefone", "Série"]
+
+nome_arquivo= "matricula_" + aluno["Nome completo"]+".csv"
+
+with open(nome_arquivo, mode="a",newline="") as arquivo:
+    escritor = csv.DictWriter(arquivo, fieldnames=colunas)
+    escritor.writeheader()
+    escritor.writerow(aluno)
+
